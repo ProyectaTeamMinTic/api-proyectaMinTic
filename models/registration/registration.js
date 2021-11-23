c450444e6afa1211989a244c9d67d19ba5a9fca8
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2021-11-22 22:07:07 -0500

    Se agrega resolvers, types y Schema para registration

--- models/registration/registration.js
+++ models/registration/registration.js
@@ -1,9 +1,41 @@
 //IMPORTS
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+import { ProjectModel } from "../project/project.js";
+import { UserModel } from "../user/user.js";
 
 //DEFINIR ESQUEMAS
+const { Schema, model } = mongoose;
 
 //METODOLOGIA 4 - VIRTUAL POPULATE (DESDE EL LADO 1 DE LA RELACIÓN)
+const registrationSchema = new Schema({
+  estado: {
+    type: String,
+    enum: ["ACEPTADA", "RECHAZADA", "PENDIENTE"],
+    required: true,
+  },
+  fechaIngreso: {
+    type: Date,
+    required: true,
+  },
+  fechaEgreso: {
+    type: Date,
+    required: true,
+  },
+  proyecto: {
+    type: Schema.Types.ObjectId,
+    ref: ProjectModel,
+    required: true,
+  },
+  estudiante: {
+    type: Schema.Types.ObjectId,
+    ref: UserModel,
+    required: true,
+  },
+});
 
 //DEFINIR MODELO DEL OBJETO
+const registrationModel = model("Registration", registrationSchema);
 
 //EXPORTACIÓN DEL MODELO
+export { registrationModel };
--- models/registration/resolvers.js
+++ models/registration/resolvers.js
@@ -1,9 +1,39 @@
 //IMPORTS
+import { registrationModel } from "./registration.js";
 
 //RESOLVER{
 
-//  DEFINICION DE QUERY
-
-//  DEFINICIÓN DE MUTACIONES }
+const registrationResolvers = {
+  //  DEFINICION DE QUERY
+  Query: {
+    Registrations: async (parent, args) => {
+      const registrations = await registrationModel.find();
+      return registrations;
+    },
+  },
+  //  DEFINICIÓN DE MUTACIONES
+  Mutation: {
+    createRegistration: async (parent, args) => {
+      const registrationCreated = await registrationModel.create({
+        estado: args.estado,
+        proyecto: args.proyecto,
+        estudiante: args.estudiante,
+      });
+      return registrationCreated;
+    },
+    approveRegistration: async (parent, args) => {
+      const registrationApproved = await registrationModel.findByIdAndUpdate(
+        args.id,
+        {
+          estado: "ACEPTADO",
+          fechaIngreso: Date.now(),
+        },
+        { new: true }
+      );
+      return registrationApproved;
+    },
+  },
+};
 
 //EXPORT
+export { registrationResolvers };
